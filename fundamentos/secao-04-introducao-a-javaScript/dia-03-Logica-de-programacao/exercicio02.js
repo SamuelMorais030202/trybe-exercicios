40ecf29bd60a35f32c580a42374b0463c48f5700
Samuel Morais030202 <moraissamuel009@gmail.com>
2022-09-23 22:45:16 -0300

    Refazendo exercício 2

--- fundamentos/secao-04-introducao-a-javaScript/dia-03-Logica-de-programacao/exercicio02.js
+++ fundamentos/secao-04-introducao-a-javaScript/dia-03-Logica-de-programacao/exercicio02.js
@@ -1,9 +1,8 @@
-let word = 'Samuel';
-let newWord = '';
-
+let word = "Samuel Morais";
+let newword = "";
+                
 for(let index = word.length - 1; index >= 0; index -= 1){
-    newWord += word[index]
-    // console.log(newWord)
-};
+    newword += word[index]
+}
 
-console.log(newWord)
+console.log(newword)
