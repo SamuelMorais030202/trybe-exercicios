9d8c76d5dda1532c252a65c06bb681e631723165
Samuel Morais030202 <moraissamuel009@gmail.com>
2022-09-29 23:51:40 -0300

    Finalizando os exercícios de fixação

--- fundamentos/secao-05-DOM-elementos-WebStorange/dia02-trabalhando-comElementos/exerciciosFixacao/script02.js
+++ fundamentos/secao-05-DOM-elementos-WebStorange/dia02-trabalhando-comElementos/exerciciosFixacao/script02.js
@@ -1,6 +1,25 @@
-let filhosDePai = document.querySelector('#pai')
+let pai = document.querySelector('#pai');
 
-let irmaoElementoOndeEsta = document.createElement('section')
-irmaoElementoOndeEsta.innerText = "estou aqui!"
+// primeiro exercício
+let section = document.createElement('section');
+section.innerText = 'Bom dia';
+section.id = 'irmaoDoElementoOndeVoceEsta';
+pai.appendChild(section);
 
-filhosDePai.appendChild(irmaoElementoOndeEsta)
+// segundo exercício
+let elementoOndeVoceEsta = document.querySelector('#elementoOndeVoceEsta');
+let filhoDoElementoOndeVoceEsta = document.createElement('h1');
+filhoDoElementoOndeVoceEsta.innerText = 'Xablau';
+filhoDoElementoOndeVoceEsta.style.color = 'red';
+elementoOndeVoceEsta.appendChild(filhoDoElementoOndeVoceEsta);
+
+// terceiro exercício
+let primeiroFilhoDoFilho = document.querySelector('#primeiroFilhoDoFilho');
+let subtitulo = document.createElement('h4');
+subtitulo.innerText = 'Olá mundo';
+subtitulo.style.color = 'blue';
+primeiroFilhoDoFilho.appendChild(subtitulo);
+
+// quarto exercício
+let terceiroFilho = subtitulo.parentElement.parentElement.nextElementSibling;
+terceiroFilho.innerHTML = 'Bora praticar';
