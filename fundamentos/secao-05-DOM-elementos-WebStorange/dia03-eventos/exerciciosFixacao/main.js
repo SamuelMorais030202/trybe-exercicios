b838bf12001562966560466bfcca0eb26e2e5b9f
Samuel Morais030202 <moraissamuel009@gmail.com>
2022-10-01 00:28:58 -0300

    finalizando exercícios de fixação

--- fundamentos/secao-05-DOM-elementos-WebStorange/dia03-eventos/exerciciosFixacao/main.js
+++ fundamentos/secao-05-DOM-elementos-WebStorange/dia03-eventos/exerciciosFixacao/main.js
@@ -4,21 +4,47 @@ const thirdLi = document.getElementById('third-li');
 const input = document.getElementById('input');
 const myWebpage = document.getElementById('my-spotrybefy');
 
-// - Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
-//  - Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+window.onload = function () {
 
-// - Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
-// com a classe 'tech';
+  // - Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
+  //  - Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
 
-// - Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
-// redirecione para alguma página;
-//  - Que tal redirecionar para seu portfólio?
+  function adicionandoClass (event) {
+    let classe = document.querySelector('.tech');
+    classe.classList.remove('tech');
+    event.target.classList.add('tech');
+  }
 
-// - Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
-// a cor do mesmo;
+  firstLi.addEventListener('click', adicionandoClass);
+  secondLi.addEventListener('click', adicionandoClass);
+  thirdLi.addEventListener('click', adicionandoClass);
+
+  // - Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
+  // com a classe 'tech';
+  
+  input.addEventListener('input', function(event) {
+    const elemento = document.querySelector('.tech')
+    elemento.innerHTML = event.target.value
+  })
+
+  // - Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
+  // - redirecione para alguma página;
+  // - Que tal redirecionar para seu portfólio?
+
+  myWebpage.addEventListener('dblclick', function () {
+    window.location.replace('https://github.com/SamuelMorais030202')
+  })
+
+  myWebpage.addEventListener('mouseover', function (event) {
+    event.target.style.color = 'orange';
+  })
+
+  myWebpage.addEventListener('mouseleave', function (event) {
+    event.target.style.color = 'white'
+  })
+}
 
 // Segue abaixo um exemplo do uso de event.target:
-
 
 function resetText(event) {
   // O Event é passado como um parâmetro para a função.
