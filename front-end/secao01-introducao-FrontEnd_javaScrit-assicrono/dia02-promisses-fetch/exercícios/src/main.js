4b3d63256aa226f2577620cafb52bf1a05581089
Samuel Morais030202 <moraissamuel009@gmail.com>
2022-11-14 14:59:09 -0300

    Desenvolvendo conteúdo do dia

--- front-end/secao01-introducao-FrontEnd_javaScrit-assicrono/dia02-promisses-fetch/exercícios/src/main.js
+++ front-end/secao01-introducao-FrontEnd_javaScrit-assicrono/dia02-promisses-fetch/exercícios/src/main.js
@@ -1,6 +1,7 @@
 fetch("https://dummyjson.com/users")
 .then((result) => result.json())
 .then((data) => fillUsersSelect(data.users))
+.catch((error => console.log('Erro ao fazer a requisição.', error.message )))
 
 const fillUsersSelect = (informationsPeoples) => {
   console.log(informationsPeoples);
