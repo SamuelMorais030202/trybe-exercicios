f23c66bfe05294ca05205ce4444ad9e4e32f09ab
Samuel Morais030202 <moraissamuel009@gmail.com>
2023-01-19 16:54:36 -0300

    Avançando no conteúdo de mock

--- front-end/Secao06-testes-automatizados-RTl/dia02-Moks/exemplos-moks/src/App.test.js
+++ front-end/Secao06-testes-automatizados-RTl/dia02-Moks/exemplos-moks/src/App.test.js
@@ -1,8 +1,29 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+describe('Testando moks', () => {
+  function num () {
+    return Math.floor(Math.random() * 100);
+  }
+  function soma () {
+    return num() + num()
+  }
+
+  it('Iniciando com moks', () => {
+    num = jest.fn().mockReturnValue(8);
+
+    expect(num()).toBe(8);
+  });
+ 
+  it('Testando a função de soma', () => {
+    num = jest.fn().mockReturnValue(2);
+
+    expect(soma()).toBe(4);
+  })
+})
