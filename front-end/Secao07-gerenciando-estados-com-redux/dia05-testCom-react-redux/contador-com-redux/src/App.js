fe5dd115605319d0245ac60bbeaed3c93bba28c1
Samuel Morais030202 <moraissamuel009@gmail.com>
2023-02-01 10:22:01 -0300

    Terminando configuração do redux do dia 5 da seção 7 de front-end

--- front-end/Secao07-gerenciando-estados-com-redux/dia05-testCom-react-redux/contador-com-redux/src/App.js
+++ front-end/Secao07-gerenciando-estados-com-redux/dia05-testCom-react-redux/contador-com-redux/src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { actionCount } from './redux/actions';
+import { actionCount, actionClick } from './redux/actions';
 
 class App extends React.Component {
   render () {
-    const { countState, dispatch } = this.props;
-    console.log(countState);
+    const { countState, dispatch, clicksState } = this.props;
+    
+    const dispathAction = (add = 1) => {
+      dispatch(actionCount(add));
+      dispatch(actionClick());
+    } 
+
+    const numberClicks = `Número de clicks: ${clicksState}`;
     return (
       <div>
         <h1>Contador</h1>
         <h2>{ countState }</h2>
-        <button onClick={() => dispatch(actionCount(1))}>Incrementar 1</button>
-        <button onClick={() => dispatch(actionCount(5))}>Incrementar 5</button>
+        <button onClick={() => dispathAction()}>Incrementar 1</button>
+        <button onClick={() => dispathAction(5)}>Incrementar 5</button>
+        <h3>
+          { numberClicks }
+        </h3>
       </div>
     )
   }
 }
 
 const mapStateToProps = (state) => ({
-  countState: state.count,
+  countState: state.counterReducer.count,
+  clicksState: state.counterReducer.clicks,
 })
 
 export default connect(mapStateToProps)(App);
--- front-end/Secao07-gerenciando-estados-com-redux/dia05-testCom-react-redux/contador-com-redux/src/redux/actions/index.js
+++ front-end/Secao07-gerenciando-estados-com-redux/dia05-testCom-react-redux/contador-com-redux/src/redux/actions/index.js
@@ -2,3 +2,7 @@ export const actionCount = (increment = 1) => ({
   type: 'INCREMENT_COUNTER',
   payload: increment,
 });
+
+export const actionClick = () => ({
+  type: 'INCREMENT_CLICK',
+})
